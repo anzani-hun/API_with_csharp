406a80642688878fe99954d8692e52a6493d2f69
anzani-hun <anzani@hun>
2024-03-05 16:52:37 +0100

    Add project files.

---
+++ API_with_csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_with_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program Vége!");
+
+            Console.ReadLine();
+        }
+    }
+}
